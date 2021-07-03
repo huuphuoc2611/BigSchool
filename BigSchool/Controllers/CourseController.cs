867dc47116ba93ff495db58f376a39d28ae20145
huuphuoc2611 <84374523+huuphuoc2611@users.noreply.github.com>
2021-07-03 15:05:47 +0700

    Add project files.

---
+++ BigSchool/Controllers/CourseController.cs
@@ -0,0 +1,45 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course course)
+        {
+
+            BigSchoolContext context = new BigSchoolContext();
+            ModelState.Remove("LectureId");
+            if (!ModelState.IsValid)
+            {
+                course.ListCategory = context.Categories.ToList();
+                return View("Create", course);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            course.LecturerId = user.Id;
+            context.Courses.Add(course);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
